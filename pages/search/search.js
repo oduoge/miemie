fa1e2be20f232e8f253eb45b9fb546bd12f2f360
oduoge <oduoge@126.com>
2021-01-08 17:48:09 +0800

    搜索页面添加语言选框

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,34 +1,20 @@
+const baidu = require('../../utils/api/baidu.js')
 Page({
   data: {
-    imgUrl: '/images/fake/joke.png'
+    imgUrl: '/images/fake/joke.png',
+    option1: [],
+    option2: [],
+    value1: 'en',
+    value2: 'zh',
   },
 
-  search: e => {
+  search: function(e) {
     let content = e.detail.value
     wx.showModal({
       title: '提示',
       content: '对不起，还没这功能',
       showCancel: false
     })
-    // wx.request({
-    //   url: `https://api.shanbay.com/bdc/search/?word=${content}`,
-    //   data: {},
-    //   method: 'GET',
-    //   success: res => {
-    //     const { msg } = res.data
-    //     if (msg == "SUCCESS") {
-    //       wx.navigateTo({
-    //         url: `./detail/detail?content=${content}`
-    //       })
-    //     } else {
-    //       wx.showModal({
-    //         title: '提示',
-    //         content: '对不起，查询不到该词信息',
-    //         showCancel: false
-    //       })
-    //     }
-    //   },
-    // })
   },
 
   help: () => {
@@ -40,20 +26,49 @@ Page({
   },
 
   onLoad: function(options) {
-    wx.request({
-      url: 'https://api.ixiaowai.cn/gqapi/gqapi.php?return=json',
-      data: {},
-      method: 'GET',
-      success: res => {
-        console.log(res.data)
-        if ('200' == res.data.code) {
-          this.setData({
-            imgUrl: res.data.imgurl
-          })
-        } else {
+    this.initImage()
+    this.initLangMenu()
+  },
 
+  initImage: function (){
+    let url = wx.getStorageSync('searchImageUrl')
+    if (url) {
+      this.setData({
+        imgUrl: url
+      })
+    } else {
+      wx.request({
+        url: 'https://api.ixiaowai.cn/gqapi/gqapi.php?return=json',
+        data: {},
+        method: 'GET',
+        success: res => {
+          console.log(res.data)
+          if ('200' == res.data.code) {
+            this.setData({
+              imgUrl: res.data.imgurl
+            })
+            wx.setStorage({
+              key: 'searchImageUrl',
+              data: res.data.imgurl
+            })
+          }
         }
-      }
+      })
+    }
+  },
+  
+  initLangMenu: function() {
+    let op1 = []
+    for (let item in baidu.commonLang) {
+      op1.push({
+        text: item,
+        value: baidu.commonLang[item]
+      })
+    }
+    console.log(op1)
+    this.setData({
+      option1: op1,
+      option2: op1
     })
   }
 })
